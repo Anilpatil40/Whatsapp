e6181ed5641ddf16dbad93c7cfeed2d61db42064
panil8993 <panil8993@gmail.com>
2020-12-16 17:19:22 +0530

    Initial commit

---
+++ app/src/main/java/com/swayam/whatsapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.swayam.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.random.customdialog.CustomDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+}
